128defb755a9a139e393748031ec850f50640dfc
Peng Ye <yep0608@hotmail.com>
2019-10-08 21:53:54 +1000

    08/10

--- src/app.js
+++ src/app.js
@@ -49,6 +49,22 @@ app.get('/weather', (req, res) => {
   })
 })
 
+app.get('/help/*', (req,res) => {
+  res.render('404', {
+    errorMessage: 'Help article not found',
+    title: '404',
+    name: 'Peng Ye'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    errorMessage: 'Page not found.',
+    title: '404',
+    name: 'Peng Ye'
+  })
+})
+
 app.listen(3000, () => {
   console.log('Server is up on port 3000.')
 })
