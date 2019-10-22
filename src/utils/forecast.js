78b79b644f54c3f6d80b8942761e4be9075dcb40
Peng Ye <yep0608@hotmail.com>
2019-10-22 21:00:01 +1000

    Dev Setting

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,6 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location!', undefined)
     } else {
-      console.log(url)
       callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degree out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
     }
   })
